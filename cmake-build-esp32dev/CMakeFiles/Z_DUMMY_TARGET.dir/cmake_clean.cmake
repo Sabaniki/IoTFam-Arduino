f0ef67e31f741a461732b1494352f4fab5624867
Sabaniki <sawada13@mccc.jp>
2020-06-25 10:22:07 +0900

    Initial commit

---
+++ cmake-build-esp32dev/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/esp_wroom_02/PubSubClient_ID89/src/PubSubClient.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.o"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
